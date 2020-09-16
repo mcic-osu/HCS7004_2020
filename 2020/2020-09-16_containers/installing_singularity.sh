08ff42744072b302939e1896d31034d1481397b8
jelmerp <jelmerpoelstra@gmail.com>
2020-09-16 19:41:16 -0400

    Update install script

--- 2020/2020-09-16_containers/installing_singularity.sh
+++ 2020/2020-09-16_containers/installing_singularity.sh
@@ -5,26 +5,27 @@ set -e
 sudo apt-get update
 sudo apt-get install -y \
     build-essential libssl-dev uuid-dev libgpgme11-dev squashfs-tools libseccomp-dev \
-    wget pkg-config git cryptsetup-bin automake checkinstall zlib1g-dev libtbb-dev 
+    wget pkg-config git cryptsetup-bin automake checkinstall zlib1g-dev libtbb-dev
 
 # Install Go
 ## Another dependency for Singularity is the scripting language Go, and we'll need to manually install that:
-export VERSION=1.13 OS=linux ARCH=amd64 
+export VERSION=1.13 OS=linux ARCH=amd64
 wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz
 sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz
-rm go$VERSION.$OS-$ARCH.tar.gz 
+rm go$VERSION.$OS-$ARCH.tar.gz
 echo 'export PATH=/usr/local/go/bin:${PATH}:${HOME}/go/bin' >> ~/.bashrc
-source ~/.bashrc 
+source ~/.bashrc
+export PATH=/usr/local/go/bin:${PATH}:${HOME}/go/bin
 go get -u github.com/golang/dep/cmd/dep
 
 # Install Singularity
 ## Now, we're ready to install Singularity. We'll download it from Github,
-## a repository for software and scripts linked to the "git" version control system. 
-git clone https://github.com/sylabs/singularity.git 
+## a repository for software and scripts linked to the "git" version control system.
+git clone https://github.com/sylabs/singularity.git
 cd singularity
 git fetch
 ./mconfig
-make -C ./builddir 
+make -C ./builddir
 sudo make -C ./builddir install
 . /usr/local/etc/bash_completion.d/singularity
 cd ..
